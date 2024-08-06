f7d05069ed8f11cdea6ee22c9c7a84fb98bc2736
kailash kumar <kailash414243@gmail.com>
2024-08-06 23:45:15 +0530

    learning about js variables

---
+++ 4-variable.js
@@ -0,0 +1,45 @@
+// Variables in javascript
+
+// let
+// const
+// var
+
+// let numOne = 10;
+// console.log(numOne);
+
+// let firstName = "Kailash";
+// let lastName = "Kumar";
+// console.log(firstName, lastName);
+// console.log(firstName + lastName);
+
+// let userIntro = "My name is Kailash kumar";
+// console.log(userIntro);
+// console.log(typeof userIntro);
+
+// let age = 25;
+// console.log(typeof age);
+
+// let a = undefined;
+// console.log(a);
+// a = 10;
+// console.log(a);
+
+// let b;
+// console.log(b);
+
+// firstName = 'aryan'
+// console.log(firstName)
+
+// const hoursInDay = 24
+// console.log(hoursInDay)
+
+// hoursInDay=26 //error, you cannot changed
+// console.log(hoursInDay)
+
+// {
+//     let numberOne=10
+//     console.log(numberOne)
+// }
+
+// console.log(numberOne)
+
