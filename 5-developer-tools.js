ea249c29a33c3e5552d754134f240ef8c8a5351f
kailash kumar <kailash414243@gmail.com>
2024-08-07 23:37:41 +0530

    learn abour chrome developer tools

---
+++ 5-developer-tools.js
@@ -0,0 +1,13 @@
+var firstName = "Kailahs";
+let lastName = "kumar";
+
+let age = 15;
+
+// console.log(typeof yearOfBirth)
+
+const yearOfBirth = 1999;
+console.log(yearOfBirth)
+console.log(typeof yearOfBirth)
+
+let userIntro = "Hi, my name is " + firstName + " " + lastName
+
