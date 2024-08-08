45a3f8ed0fa21487de3dbc73ca2543841416b14b
kailash kumar <kailash414243@gmail.com>
2024-08-08 22:40:09 +0530

    js dialog box understanding

---
+++ 6-dialog-box.js
@@ -0,0 +1,19 @@
+// alert()
+
+// alert('Hello world')
+
+// confirm('Okay')
+
+// const isConfirmed = confirm("WOuld u like to proced")
+
+// if(isConfirmed==true){
+//     alert("True yes done")
+// }else{
+//     alert('Not completed')
+// }
+
+
+// let userInput = prompt()
+// if(userInput==null){
+//     console.log('Null value assigned because not input by user')
+// }
