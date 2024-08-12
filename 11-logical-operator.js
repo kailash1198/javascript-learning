aa7c37db8e450ad242d6066f084addd0a65c0c11
kailash kumar <kailash414243@gmail.com>
2024-08-12 22:09:29 +0530

    logical operator

---
+++ 11-logical-operator.js
@@ -0,0 +1,68 @@
+// // Logical operator
+// // &&, ||, !
+
+// let numOne = 10;
+// let numTwo = 20;
+
+// if (numOne == 0 && numTwo == 0) {
+//   console.log("True");
+// } else {
+//   console.log("False");
+// }
+
+// const userAge = 25;
+// if (userAge >= 18 && userAge <= 24) {
+//   console.log("True");
+// } else {
+//   console.log("False");
+// }
+
+// const one = 1;
+// const two = 2;
+
+// if (one == 1 || two == 2) {
+//   console.log("True");
+// } else {
+//   console.log("False");
+// }
+
+// if (!one) {
+//   console.log("True");
+// } else {
+//   console.log("False");
+// }
+
+// const userAge = 22;
+// const isSchoolStudent = userAge >= 5 && userAge <= 18;
+// const isCollegeStudent = userAge >= 18 && userAge <= 24;
+
+// const isStudent = isSchoolStudent || isCollegeStudent
+
+// const test = Boolean(0&2)
+// console.log(test)
+
+// const andResult = '' && 'Hello'
+// console.log(andResult)
+
+// function ageCheck(age){
+//     if(age==age){
+//         console.log(`Your age is: ${age}`)
+//     }
+// }
+
+
+// ageCheck(25)
+
+// const one = 'Hello' && 4+8
+// console.log(one)
+
+// const two = 'Hello' || 4+8
+// console.log(two)
+
+// const logResult = console.log('Hello')
+// console.log(logResult)
+
+const one=null
+const two=2
+
+console.log(!one)
