2beeceb81ea84bdd0bd8ed2f6290514e42802a4c
kailash kumar <kailash414243@gmail.com>
2024-08-13 21:41:42 +0530

    decision making state,emt

---
+++ 13-decision-making.js
@@ -0,0 +1,18 @@
+// const userAge = 28
+// const userName = "Kailash"
+
+// console.log(`User name : ${userName}`)
+// console.log(`User Age: ${userAge}`)
+
+// if(userAge==28){
+//     console.log('User age is : ', userAge)
+// }
+
+const userAge = prompt('Please enter your age : ')
+if(userAge==18){
+    console.log('Your age is 18')
+}else{
+    console.log('Your age is not 18')
+}
+
+debugger //for debug the code in chrome developer tool
