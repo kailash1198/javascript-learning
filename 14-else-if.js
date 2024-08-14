3999046699f40edd46ce9d3df75c1727e928fe96
kailash kumar <kailash414243@gmail.com>
2024-08-14 23:37:01 +0530

    if alse and switch statemenmt

---
+++ 14-else-if.js
@@ -0,0 +1,12 @@
+const userAge = 10;
+const userName = "kailash1198";
+
+if (userAge >= 10) {
+  if (userName == "kailash1198") {
+    console.log("Log in sucess");
+  } else {
+    console.log("Invalid user name");
+  }
+} else {
+  console.log("Invalid user age");
+}
---
+++ 15-switch-case.js
@@ -0,0 +1,17 @@
+const userAge = 5;
+switch (userAge) {
+  case 1:
+    console.log("Age is 1");
+
+  case 2:
+    console.log("Age is 2");
+
+  case 3:
+    console.log("Age is 3");
+
+  case 4:
+    console.log("Age is 4");
+
+  case 5:
+    console.log("Age is 5")
+}
