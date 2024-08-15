eccf96bb784140b60fc5fbf5a31a346984ba2bf2
kailash kumar <kailash414243@gmail.com>
2024-08-15 23:01:55 +0530

    ternary operator in js

---
+++ 16-ternary-operator.js
@@ -0,0 +1,7 @@
+// Ternary operator - alternative of switch and if else statement
+
+const numOne = 10;
+const numTwo = 2;
+
+const result = numOne < numTwo ? "True" : "False";
+console.log(result);
