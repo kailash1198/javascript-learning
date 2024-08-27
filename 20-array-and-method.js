64f2d6c70c3370483e7b24eb66ba7f956ed164f3
kailash kumar <kailash414243@gmail.com>
2024-08-28 00:02:23 +0530

    array and its method

---
+++ 20-array-and-method.js
@@ -0,0 +1,78 @@
+// const myArray = [1,2,3,4,5]
+// console.log(myArray)
+
+// myArray[0]=10
+// console.log(myArray[0])
+
+// let userName = ['User-1', 'User-2', 'User-3', 'User-4']
+// console.log(userName)
+// console.log(userName[0])
+// console.log(userName[1])
+// console.log(userName[2])
+// console.log(userName[3])
+
+// console.log(userName.indexOf('User-4'))
+
+// let searc_value = 'User-2'
+
+// let studentList = ['Ram', 'Shyam', 'Mohan']
+// let userList = ['User-1', 'User-2', 'User-3']
+
+// let arrayList = [studentList, userList]
+
+// console.log(studentList)
+// console.log(arrayList[0][2])
+// console.log(arrayList[1][1])
+
+// console.log(arrayList)
+// console.log(arrayList[2][0])
+// console.log(arrayList[2][1])
+// console.log(arrayList[2][2])
+
+// let newArray = new Array(1,2,3,4,5)
+// console.log(newArray)
+// console.log(newArray[0])
+
+let staffList = ["Staff-1", "Staff-2", "Staff-3"];
+let newArray = ["User-1", "User-2", "User-3"];
+// ➡️ Array Methods
+// staffList.push('Staff-4')
+// staffList.pop()
+// staffList.shift()
+// staffList.unshift('NewStaff-unshift')
+// console.log(staffList.length)
+// let jointArray = staffList.concat(newArray)
+// let sliceArray = staffList.slice(2)
+// console.log(staffList.join())
+// console.log(newArray.join())
+
+//➡️ For Each method
+// let fruits = ["apple", "banana", "mango", "Orange"];
+// fruits.forEach(fruit => console.log(fruit));
+
+// let students = ['Ram', 'Shyam', 'Kailash']
+// students.forEach(function(name){
+//     console.log(name)
+// })
+// students.forEach(function(){
+//     console.log("Hello world")
+// })
+
+//➡️ map() method
+// let numberList = ["User-1", "User-2", "User-3"];
+// numberList.map(function (element) {
+//   let newArray = element.toUpperCase;
+// });
+
+//➡️ filter() method
+// let arrayOne = ['Element-1', 'Element-2', 'Element-3', 'Element-4']
+// arrayOne.filter(function(element){
+//     let newArray=element.search('Element-3')
+//     console.log(newArray)
+// })
+
+
+
+// console.log(staffList)
+// console.log(jointArray)
+// console.log(sliceArray)
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,1 @@
-console.log('kailash
-//     new line'); 
+name
