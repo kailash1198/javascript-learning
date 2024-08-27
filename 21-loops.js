2ecd7959bf5bb527ae1d6df22999c1dc5111fb82
kailash kumar <kailash414243@gmail.com>
2024-08-28 00:15:58 +0530

    loop practice

---
+++ 21-loops.js
@@ -0,0 +1,20 @@
+// Loops in JS
+
+//While loop ➡️
+// let numberList = 10
+// while(numberList>=0){
+//     console.log(numberList)
+//     numberList--
+// }
+
+//do while loop ➡️
+// do {
+//     console.log(numberList)
+//     numberList--
+// } while (numberList>=0);
+
+//for loop ➡️
+// let myArray=[1,2,3,4,5,6,7,8,9]
+// for(let i=0; i<myArray.length; i++){
+//     console.log(myArray[i])
+// }
