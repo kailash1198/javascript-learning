ffb9f9edff64a25400ab1d8178cba4dd8ea19b41
kailash kumar <kailash414243@gmail.com>
2024-08-28 23:58:16 +0530

    function practice

---
+++ 22-function.js
@@ -0,0 +1,70 @@
+//function -
+// 1. User defined Function
+// 2. In built Function
+
+// ▶️ User defined Function
+// ============= TYPES OF USER DEFINED FUNCTION ==============
+// 1️⃣ Simple Function/Function Declaration
+// function addSum(numberOne, numberTwo) {
+//   let sum = numberOne + numberTwo;
+//   return sum;
+// }
+
+// function subtract(a, b) {
+//   c = b - a;
+//   return c;
+// }
+// 2️⃣ Function Expression
+// const myFunction = function () {
+//   console.log("Hello World, This is my Functtion");
+// }
+
+// myFunction()clg
+
+// 3️⃣ Arrow Function
+// const arrowFunction = () => {
+//   console.log("This is arrow function call");
+// };
+// arrowFunction();
+
+// 4️⃣ Anonymous Function
+// setTimeout(function () {
+//   console.log("This is an anonymous function.");
+// }, 1000);
+
+// setTimeout(function myNamedFunction() {
+//   console.log(
+//     "This is a named function, but it's still an anonymous function because it's not being called directly."
+//   );
+// }, 1000);
+
+// 5️⃣ Immediately Invoked Function
+// (function () {
+//   console.log("Immediate call function");
+// })();
+
+// (function(){
+// console.log('Immediate call-2')
+// })()
+
+// ========== FUNCTION CALL ===========
+// let numOne = 10;
+// let numTwo = 20;
+// let totalSum = addSum(numOne, numTwo);
+// let subtractNum = subtract(numOne, numTwo);
+
+// console.log(totalSum);
+// console.log(subtractNum);
+
+// ===================== In Built Function ===============
+// console.log(Math.random(123))
+// console.log(Math.floor(4.1))
+
+// let currentDate = new Date()
+// console.log(currentDate)
+
+// let todayDay=currentDate.getDay()
+// console.log(todayDay)
+
+// let year=currentDate.getFullYear()
+// console.log(year)
