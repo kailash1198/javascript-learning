a5161d9c33890ef3be6236fc3dec186f599389ad
kailash kumar <kailash414243@gmail.com>
2024-08-30 18:05:16 +0530

    event loop

--- 28-event-loop.js
+++ 28-event-loop.js
@@ -1,2 +1,15 @@
 // Event Loop
 
+console.log("Hello-1");
+console.log("Hello-2");
+
+for (let i = 0; i < 4; i++) {
+  console.log(i);
+}
+
+//this ignore and read in last
+setTimeout(function () {
+  console.log("TimeOut function");
+}, 0);
+
+console.log("Last- hello");
