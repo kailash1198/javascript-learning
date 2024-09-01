e69c70d867f8606aa96acd5144a7f5a109e2ffec
kailash kumar <kailash414243@gmail.com>
2024-09-01 14:43:28 +0530

    select element using js

--- 55-select-tag.js
+++ 55-select-tag.js
@@ -1,1 +1,19 @@
 // Select element from HTML using javascript
+// 1. Select element by ID
+// 2. Select element by classname
+// 3. Select element by tagName
+// 4. Select element by querySelector
+// 5. Select element by QUaryAll
+
+
+// let headingOne = document.getElementById('heading-1')
+// headingOne.innerText = "Javascript Mastery"
+
+// let paragraphAll = document.getElementsByClassName('para')
+
+// paragraphAll[0].innerHTML= "KK"
+// paragraphAll[1].innerHTML= "KK"
+
+// let selectbyTagName = document.getElementsByTagName('p')
+
+// selectbyTagName[0].style.color='red'    
