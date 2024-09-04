95022871a64509dd8f161742d56b64ce32e67a43
Kailash Kumar <kailash414243@gmail.com>
2024-09-04 13:37:32 +0530

    event listener practice

--- 62-event-listener.js
+++ 62-event-listener.js
@@ -1,3 +1,37 @@
 // Event Listener
 
-const btn = document.querySelector('.btn')
+// const btn = document.querySelector('.btn')
+
+// btn.addEventListener('click', ()=>{
+//     console.log("Click Event happend")
+// })
+
+// btn.addEventListener('dblclick', ()=>{
+//     console.log("Double clicked")
+// })
+
+// window.addEventListener('resize', ()=>{
+//     alert("Window is resized")
+// })
+
+// window.addEventListener('load', ()=>{
+//     alert("All content loaded in your browser")
+// })
+
+const newTaskBtn = document.querySelector(".btn");
+const cardParent = document.querySelector(".cardParent");
+let howMany = 0;
+newTaskBtn.addEventListener("click", () => {
+  howMany=1
+  let newCheckBox = [];
+
+  for (let i = 0; i < howMany; i++) {
+    newCheckBox[i] = document.createElement("input");
+    newCheckBox[i].className = "checkBox";
+    newCheckBox[i].type = "checkbox";
+  }
+
+  for (let i = 0; i < howMany; i++) {
+    cardParent.appendChild(newCheckBox[i]);
+  }
+});
