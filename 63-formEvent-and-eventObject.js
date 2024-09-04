4cb6b12c467442390daa2cb750815cfaa66bfd1c
Kailash Kumar <kailash414243@gmail.com>
2024-09-04 20:02:47 +0530

    form event and event object practice

---
+++ 63-formEvent-and-eventObject.js
@@ -0,0 +1,23 @@
+// Form Event and Event Object
+
+// document.addEventListener('click', (e)=>{
+//     console.log(e.altKey)
+// })
+
+const inputField = document.querySelector(".input");
+const paragraph = document.querySelector('p')
+
+// inputField.addEventListener("keydown", (e) => {
+//   console.log("Key Down");
+//   console.log(e.target.value)
+//   paragraph.innerText = e.target.value
+// });
+
+// inputField.addEventListener("keyup", () => {
+//   console.log("Key Up");
+// });
+
+
+inputField.addEventListener('submit', ()=>{
+    
+})
